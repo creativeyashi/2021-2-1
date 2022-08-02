1dbcce4c970c92e62e4a7ebeba0a46b474aa05e1
Yashika Gupta <yashikagupta082@gmail.com>
2022-08-03 01:34:54 +0530

    Update Problem-3.cpp

--- Problem-3.cpp
+++ Problem-3.cpp
@@ -3,20 +3,20 @@ using namespace std;
 
 int main() 
 {
-	int a;
-	cin>>a;
+   int a;
+   cin>>a;
   
   if(a%2==0)
   {
     --a;
   }
-	int i=1;
-	while(--a)
-	{
-	    cout<<i<<", ";
-	    i += 2;
-	}
-	cout<<i;
-	return 0;
+  int count=1;
+  while(--a)
+  {
+    cout<<count<<", ";
+    count += 2;
+  }
+  cout<<count;
+  return 0;
 }
 
